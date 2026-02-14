23ab0145b579139c6c36320edbe26cb0ffd028f7
Hudker, Rizzco CEO <hudsonjackharker@gmail.com>
2026-02-14 13:30:34 -0700

    Create install.sh for gpio_fan_setup installation
    Add installation script for gpio_fan_setup

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Must be root
+if [ "$EUID" -ne 0 ]; then
+    echo "This installer must be run as root."
+    exit 1
+fi
+
+# Install the script
+cp fan_setup.py /usr/bin/gpio_fan_setup
+chmod +x /usr/bin/gpio_fan_setup
+
+echo "Installed gpio_fan_setup to /usr/bin"
